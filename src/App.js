de0c1f3992a2c2e805c8f5f2fb097e87f34b433e
Brian McPhillips <B.McPhillips89@gmail.com>
2020-08-09 18:27:19 -0700

    Photos are rendered to page

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import ImageList from './imageList.js';
 import './App.css';
 import Header from './Header.js';
 import images from './data.js';
-import { render } from '@testing-library/react';
+
 
 function App() {
   return (
    <>
     <Header />
-    <ImageList />
+    <ImageList data={images}/>
    </>
   );
 }
--- src/imageList.js
+++ src/imageList.js
@@ -1,12 +1,34 @@
 import React from 'react';
-import ImageItem from './imageItem';
+
 
 
 export default class ImageList extends React.Component {
     render() {
         return ( 
-                <div> {this.props.images.map((image) => <ImageItem key={image.title} image={image.url} />)} </div>
+            <section>
+                <div className="image-container">
+                    <ul>
+                        {this.props.data.map(item => 
+                            <ImageItem title={item.title} picture={item.url}/>
+                        )}
+                    </ul>
+                </div>
+            </section>    
         )
     }
+}
 
+class ImageItem extends React.Component {
+    render() {
+        const {title, picture} = this.props
+        return (
+            <div className="image">
+                <li>
+                    <h2>{title}</h2>
+                    <img style={{height:'200px', width: '200px'}} src={picture} alt={title}/>
+                </li>
+                
+            </div>
+        )
+    }
 }
